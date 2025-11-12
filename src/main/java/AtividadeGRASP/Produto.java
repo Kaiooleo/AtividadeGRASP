1203552f524bb8dfacf874066aca9240431915f3
leona <leona@LAPTOP-HR6JRF1H>
2025-11-12 13:25:09 -0300

    Commit inicial do projeto

---
+++ src/main/java/AtividadeGRASP/Produto.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package AtividadeGRASP;
+
+/**
+ *
+ * @author leona
+ */
+public class Produto {
+
+    private String nome;
+    private double preco;
+    
+    
+}
