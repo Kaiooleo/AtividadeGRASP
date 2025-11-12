f060e13c4001aeb44d7ace897427361ce234005f
leona <leona@LAPTOP-HR6JRF1H>
2025-11-12 13:43:10 -0300

    Adicionado classe Produto

--- src/main/java/AtividadeGRASP/Produto.java
+++ src/main/java/AtividadeGRASP/Produto.java
@@ -8,10 +8,33 @@ package AtividadeGRASP;
  *
  * @author leona
  */
+
+//classe produto e seus atributos, nome e preço
 public class Produto {
 
     private String nome;
     private double preco;
     
+    //construtor
+    public Produto(String nome, double preco){
+     this.nome = nome;
+     this.preco = preco;
+    }
     
+    //Getters e Setters
+    
+    public String getNome() {
+        return nome;
+    }
+    public double getPreco() {
+        return preco;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
 }
